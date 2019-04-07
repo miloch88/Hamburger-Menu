ca048fe9c980ea041424a2f7fc0f24406c3dd487
Miloch <michal.miloch@gmail.com>
2019-04-07 12:10:59 +0200

    Last Commit

--- js/script.js
+++ js/script.js
@@ -1,6 +1,20 @@
-document.querySelector('.burger').addEventListener('click', function () {
-    document.querySelector('.fa-bars').classList.toggle('show');
-    document.querySelector('.fa-times').classList.toggle('show');
+// document.querySelector('.burger').addEventListener('click', function () {
+//     document.querySelector('.fa-bars').classList.toggle('show');
+//     document.querySelector('.fa-times').classList.toggle('show');
+//     document.querySelector('aside').classList.toggle('show');
+//     document.querySelector('.wrapper').classList.toggle('show');
+// })
 
-    document.querySelector('aside').classList.toggle('show');
+// $('.burger').on('click', function(){
+//     $('.fas, aside, .wrapper').toggleClass('show');
+// });
+
+const activeElement = document.querySelectorAll('.active');
+
+document.querySelector('.burger').addEventListener('click', function () {
+
+    
+    for (let i = 0; i < activeElement.length; i++) {
+        activeElement[i].classList.toggle('show');
+    }
 })
